be70763aa9b62f8daaf3cf9954b1fad3f24289c8
Denis Sanchez <86267586+DenisV2112@users.noreply.github.com>
2025-04-21 18:00:42 -0500

    Add files via upload

---
+++ example.py
@@ -0,0 +1,6 @@
+print("Hola mundo")
+
+age = input ("Insert your age: ")
+if(not age == int):{print("inserte un caracter valido")}
+elif ( int(age) <= 18): {print("eres menor de edad")} 
+elif ( int(age) >= 18): {print("eres mayor de edad")}
