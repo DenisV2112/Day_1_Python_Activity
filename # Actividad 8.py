1998a9dfab875615ceecf031930f3cfeefef1cef
Denis Sanchez <86267586+DenisV2112@users.noreply.github.com>
2025-04-21 21:30:55 -0500

    Update # Actividad 8.py

--- # Actividad 8.py
+++ # Actividad 8.py
@@ -1,7 +1,7 @@
 # Actividad 8
-peso = float(input("Ingrese su peso en kilogramos: "))
-altura = float(input("Ingrese su altura en metros: "))
-imc = peso / (altura ** 2)
+peso = input("Ingrese su peso en kilogramos: ")
+altura = input("Ingrese su altura en metros: ")
+imc = float(peso) / (float(altura) ** 2)
 if (imc < 18.5):{print ("Bajo peso")}
 elif 18.5 <= imc <= 24.9:{ print ("Peso normal")}
 elif 25 <= imc <= 29.9:{ print ("Sobrepeso")}
