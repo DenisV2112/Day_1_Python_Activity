4ca3b688543954ed2be6bc05b64680b7e45b1960
Denis Sanchez <86267586+DenisV2112@users.noreply.github.com>
2025-04-22 09:36:24 -0500

    Update #Actividad 9.py

--- #Actividad 9.py
+++ #Actividad 9.py
@@ -2,4 +2,4 @@
 year = input("insert a year")
 if (int(year) % 4 == 0 and int(year)% 100 != 0): print(f"{year} es un año bisiesto.")
 elif  int(year) % 400 == 0:  print(f"{year} es un año bisiesto.")
-else :  print(f"{year} es un año bisiesto.")
+else :  print(f"{year} no es un año bisiesto.")
